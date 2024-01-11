02b093a26daff1b9bd99412b862af744d0b14af5
Divjana <divjanas@hotmail.se>
2024-01-11 19:24:49 +0100

    commit

--- bookingDone.php
+++ bookingDone.php
@@ -1,4 +1,5 @@
 <?php
+
 function displayBookingInfo($guest, $arrival, $departure, $room)
 {
     global $conn;
@@ -10,6 +11,8 @@ function displayBookingInfo($guest, $arrival, $departure, $room)
 
     if ($result && $result->num_rows > 0) {
         $row = $result->fetch_assoc();
+
+
         echo "Island: " . $row['island'] . "<br>";
         echo "Hotel: " . $row['hotel'] . "<br>";
         echo "Arrival Date: " . $arrival . "<br>";
@@ -17,5 +20,26 @@ function displayBookingInfo($guest, $arrival, $departure, $room)
         echo "Total Cost: $" . $row['total_cost'] . "<br>";
         echo "Stars: " . $row['stars'] . "<br>";
         echo "Features: " . $row['features'] . "<br>";
+
+
+        $logData = array(
+            'guest' => $guest,
+            'arrival' => $arrival,
+            'departure' => $departure,
+            'room' => $room,
+            'island' => $row['island'],
+            'hotel' => $row['hotel'],
+            'total_cost' => $row['total_cost'],
+            'stars' => $row['stars'],
+            'features' => $row['features']
+        );
+
+        $logbookFile = __DIR__ . '/logbook.json';
+        $logbookData = json_decode(file_get_contents($logbookFile), true);
+
+
+        $logbookData[] = $logData;
+
+        file_put_contents($logbookFile, json_encode($logbookData, JSON_PRETTY_PRINT));
     }
 }
